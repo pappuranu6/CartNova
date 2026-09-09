9c2e73ced60225d8aab0cd1639d14fab333c1e28
Delta Student <pappuranu6@gmail.com>
2026-09-09 17:25:07 +0530

    Connect frontend to live backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import store from './store'
 import './bootstrap.min.css'
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import axios from 'axios'
 
-// axios.defaults.baseURL = 'http://35.154.127.106:5000/';
+if (process.env.REACT_APP_API_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// axios.defaults.baseURL = 'http://35.154.127.106:5000/'
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+)
+
 root.render(
   <Provider store={store}>
-    <App />;
+    <App />
   </Provider>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
