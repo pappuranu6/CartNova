26738274e2cb36453fd95cf977b0871f7e6ba8f8
Delta Student <pappuranu6@gmail.com>
2026-09-19 13:23:35 +0530

    Fix email configuration

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,29 +1,24 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
-const sendEmail = async ({
-  to,
-  subject,
-  text,
-  html,
-}) => {
+const sendEmail = async ({ to, subject, text, html }) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
 
     auth: {
-      user: 'pappuranu6@gmail.com',
-      pass: 'damjreqicbnkwzza',
+      user: "b9f963001@smtp-brevo.com",
+      pass: process.env.SMTP_PASS,
     },
-  })
+  });
 
   await transporter.sendMail({
-    from: '"CartNova" <pappuranu6@gmail.com>',
+    from: `"CartNova" <pappuranu6@gmail.com>`,
     to,
     subject,
     text,
     html,
-  })
-}
+  });
+};
 
-export default sendEmail
+export default sendEmail;
